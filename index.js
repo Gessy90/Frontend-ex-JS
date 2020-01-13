b8445aab26ea322d698b542246f68031a72fc0b2
Gessy90 <42389173+Gessy90@users.noreply.github.com>
2020-01-13 20:07:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-//FIRST METHOD
-// async function postData(url = '', customer = '') {
-//     //     const response = await fetch(url, {
-//       method: 'POST', 
-//       body: {customerID: customer} // body data type must match "Content-Type" header
-//     });
-
-//     return await response.json(); // parses JSON response into native JavaScript objects
-// }
-
-// postData('https://ln1rp.e-learningportal.com/sandbox/frontendexercise/getAttributes.cfm', 'customerA' )
-// .then((response) => {
-//     console.log(response); // JSON data parsed by `response.json()` call
-// });
-
-// var cors =https://cors-anywhere.herokuapp.com/
-//SECOND METHOD
 const url1 =
   "https://ln1rp.e-learningportal.com/sandbox/frontendexercise/getAttributes.cfm?customerID=customerA";
 
@@ -26,23 +9,10 @@ const postData1 = async () => {
   const test = await response.json();
   console.log("json", test);
 };
-// const response = fetch(url, {
-//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'cors', // no-cors, *cors, same-origin
-//     // *default, no-cache, reload, force-cache, only-if-cached
-//      // include, *same-origin, omit
-//     headers: {
-//         customerID: 'customerA'
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//      // manual, *follow, error
-//      // no-referrer, *client
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   }).then(data=>data.json()).then(response=>console.log(response));
 
-// const test = postData1();
 
-//PART 2
+// //Since I wasn't getting any data I've tried to do it this way where I was trying to work with
+// //some mock data but I didn't finish the whole process.
 const url2 =
   "https://ln1rp.e-learningportal.com/sandbox/frontendexercise/register.cfm";
 
@@ -101,18 +71,41 @@ const formInput = customerA.forEach(val => {
   input.placeholder = val.name;
 });
 
-// formId.appendChild(formInput);
 
 
-const url2 =
-  "https://ln1rp.e-learningportal.com/sandbox/frontendexercise/register.cfm";
 
 
-const postData2 = async () => {
-  const response = await fetch(url, {
-    method: "POST",headers:{customerID: 'customerA'},
-    body: JSON.stringify()
-  });
-  const test = await response.json();
-  console.log("json", test);
-};
+// //FIRST METHOD
+// // async function postData(url = '', customer = '') {
+// //     //     const response = await fetch(url, {
+// //       method: 'POST', 
+// //       body: {customerID: customer} // body data type must match "Content-Type" header
+// //     });
+
+// //     return await response.json(); // parses JSON response into native JavaScript objects
+// // }
+
+// // postData('https://ln1rp.e-learningportal.com/sandbox/frontendexercise/getAttributes.cfm', 'customerA' )
+// // .then((response) => {
+// //     console.log(response); // JSON data parsed by `response.json()` call
+// // });
+
+// // var cors =https://cors-anywhere.herokuapp.com/
+
+
+// //SECOND METHOD
+// // const response = fetch(url, {
+// //     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+// //     mode: 'cors', // no-cors, *cors, same-origin
+// //     // *default, no-cache, reload, force-cache, only-if-cached
+// //      // include, *same-origin, omit
+// //     headers: {
+// //         customerID: 'customerA'
+// //       // 'Content-Type': 'application/x-www-form-urlencoded',
+// //     },
+// //      // manual, *follow, error
+// //      // no-referrer, *client
+// //     body: JSON.stringify(data) // body data type must match "Content-Type" header
+// //   }).then(data=>data.json()).then(response=>console.log(response));
+
+// // const test = postData1();
